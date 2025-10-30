f10337327a623c680c8709c5a41b6c9faff81176
tjomss <tjomspop@gmail.com>
2025-10-30 11:25:05 +0200

    division

---
+++ src/main/java/rvt/division.java
@@ -0,0 +1,11 @@
+public class division {
+    public static void main(String[] args) {
+        int first = 3;
+        int second = 2;
+        double result = first / second;
+        System.out.println(result);
+        // ⁡⁢⁢⁢x / Double.valueOf(y)⁡ ⁡⁢⁣⁣Как поменять значение⁡
+        // ⁡⁢⁢⁢x / (double) y⁡ ⁡⁢⁣⁣2.Вариант⁡
+        // ⁡⁢⁢⁢1.0 * x / y⁡ ⁡⁢⁣⁣3.Самый прикольный⁡
+    }
+}

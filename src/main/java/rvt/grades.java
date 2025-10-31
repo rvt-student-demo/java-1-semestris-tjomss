e22e1bede8228f9d3d484ec99870d7c98776b38f
tjomss <tjomspop@gmail.com>
2025-10-31 09:26:32 +0200

    Grades

---
+++ src/main/java/rvt/grades.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+public class grades{
+    public static void main(String[] agrs) {
+    Scanner scanner = new Scanner(System.in);
+    int grade = Integer.valueOf(scanner.nextLine());
+    if (grade < 0 ) {
+        System.out.println("impossible!");
+    } 
+    else if (grade < 50){
+        System.out.println("failed");
+    }
+    else if (grade < 60){
+        System.out.println("1");
+    }
+    else if (grade < 70){
+        System.out.println("2");
+    }
+    else if (grade < 80){
+        System.out.println("3");
+    }
+    else if (grade < 90){
+        System.out.println("4");
+    }
+    else if (grade < 101){
+        System.out.println("5");
+    }
+    else if (grade > 100){
+        System.out.println("incredible!");
+    }
+    scanner.close();
+    }
+    
+}
